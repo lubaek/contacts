133df654e86eb21af9528152280cc66beeeac14a
lubaek <60066060+lubaek@users.noreply.github.com>
2022-01-17 15:43:11 +0600

    styled contact card component

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faMapMarkerAlt,
+	faPhoneAlt,
+	faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
+
 import contactImg from "../images/5fATtiHqr5I.jpg";
 
 function ContactCard({ contact }) {
@@ -9,11 +17,27 @@ function ContactCard({ contact }) {
 					<img src={contactImg} alt="contact" />
 				</div>
 			</div>
-			<h4 className="contactCard__name">{contact.name}</h4>
-			<p className="contactCard__address">{contact.address}</p>
-			<p className="contactCard__phoneNumber">{contact.phoneNumber}</p>
-			<p className="contactCard__email">{contact.email}</p>
-			<button>view contact</button>
+			<div className="contactCard__info">
+				<h4 className="contactCard__name">{contact.name}</h4>
+				<p className="contactCard__info__item">
+					<FontAwesomeIcon
+						className="contactCard__icon"
+						icon={faMapMarkerAlt}
+					/>
+					{contact.address}
+				</p>
+				<p className="contactCard__info__item">
+					<FontAwesomeIcon className="contactCard__icon" icon={faPhoneAlt} />
+					{contact.phoneNumber}
+				</p>
+				<p className="contactCard__info__item">
+					<FontAwesomeIcon className="contactCard__icon" icon={faEnvelope} />
+					{contact.email}
+				</p>
+				<Link className="contactCard__btn" to={`${contact.id}`}>
+					Show contact
+				</Link>
+			</div>
 		</div>
 	);
 }
